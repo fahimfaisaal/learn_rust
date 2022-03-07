d807228eeb9f83916a665969a0cb3dfce1651ff0
fahimfaisaal <fahimfaisal.soikot@gmail.com>
2022-03-07 22:25:43 +0600

    feat: add enum

--- custom_type/src/main.rs
+++ custom_type/src/main.rs
@@ -9,11 +9,32 @@ enum WebEvent {
     Click { x: i64, y: i64 }
 }
 
+enum Calculation {
+    Addition,
+    Subtraction,
+    Division,
+    Multiplication
+}
+
+// create a type alias
+type Operations = Calculation;
+
+impl Calculation {
+    fn run(&self, x: i64, y: i64) -> i64 {
+        match self {
+            Self::Addition => x + y,
+            Self::Subtraction => x - y,
+            Self::Division => x / y,
+            Self::Multiplication => x * y
+        }
+    }
+}
+
 fn inspect(event: WebEvent) {
     match event {
         WebEvent::PageLoad => println!("Page Loaded"),
         WebEvent::PageUpload => println!("Page Uploaded"),
-        // Destructure `c` from inside the `enum`.
+        // Destructure `letter and number` from inside the `enum`.
         WebEvent::KeyPress(letter, number) => println!("Pressed {} {}.", letter, number),
         WebEvent::Paste(str) => println!("Pasted {}", str),
         // Destructure `Click` into `x` and `y`.
